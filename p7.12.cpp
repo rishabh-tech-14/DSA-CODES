85e298a89c559d7ab239814873b3719b33b8292d
Rishabh Sajwan <rishabhsajwan48@gmail.com>
2025-07-04 19:18:59 +0530

    added median of 2 array problem

--- p7.12.cpp
+++ p7.12.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 using namespace std;
 
 // BRUTE-FORCE APPROACH
@@ -38,7 +39,7 @@ double median1(vector<int> arr1 , vector<int> arr2){
     return (double)(arr3[n/2]+(double)arr3[(n/2)-1])/2.0;
 }
 
-// OPTIMAL APPROACH
+// BETTER APPROACH
 double median2(vector<int> arr1 , vector<int> arr2){
     int n1 = arr1.size();
     int n2 = arr2.size();
@@ -47,7 +48,7 @@ double median2(vector<int> arr1 , vector<int> arr2){
     int ind2 = (n/2);
     int ind1 = (n/2)-1;
 
-    int ind1ele = -1 , ind2ele = -1;
+    double ind1ele = -1 , ind2ele = -1;
 
     int cnt = 0 , i = 0 , j = 0;
 
@@ -84,11 +85,51 @@ double median2(vector<int> arr1 , vector<int> arr2){
     return ((ind1ele+ind2ele)/2);    
 }
 
+// OPTIMAL APPROACH
+double median3(vector<int> arr1 , vector<int> arr2){
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+
+    int n = n1+n2;
+
+    if(n1>n2) return median3(arr2 ,arr1);
+
+    int left = (n1+n2+1)/2;
+
+    int low = 0 , high = n1;
+
+    while(low<=high){
+        int mid1 = (low+high)/2;
+
+        int mid2 = left-mid1;
+
+        int l1 = INT_MIN , l2 = INT_MIN;
+        int r1 = INT_MAX , r2 = INT_MAX;
+
+        if(mid1<n1) r1 = arr1[mid1];
+        if(mid2<n2) r2 = arr2[mid2];
+        if(mid1-1>=0) l1 = arr1[mid1-1];
+        if(mid2-1>=0) l2 = arr2[mid2-1];
+
+        if(l1<r2 && l2<r1){
+            if(n%2==1) return max(l1,l2);
+
+            return double(((max(l1,l2)+min(r1,r2))/2.0));
+        }
+
+        else if(l2>r1) low=mid1+1;
+        else high = mid1-1;
+    }
+
+    return 0;
+
+}
+
 int main(){
     vector<int> arr1 = {1, 4, 7, 10, 12};
-    vector<int> arr2 = {2, 3, 6, 15};
+    vector<int> arr2 = {2, 3, 6, 15,17};
 
-    cout<<median2(arr1,arr2);
+    cout<<median3(arr1,arr2);
 
     return 0;
 }

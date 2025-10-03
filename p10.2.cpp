715b28b6bd80af565f9ec0822f49fd9251aed6f1
Rishabh <rishabhsajwan48@gmail.com>
2025-10-03 18:18:02 +0530

    deletion problem in linked list

--- p10.2.cpp
+++ p10.2.cpp
@@ -49,27 +49,85 @@ node* removesHead(node* head){
 }
 
 node* removesTail(node* head){
-    if(head == NULL || head->next == NULL) return NULL;
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+
     node* temp = head;
 
     while(temp->next->next!=NULL){
         temp = temp->next;
     }
 
-    delete temp->next;
+    free(temp);
 
     temp->next = nullptr;
 
     return head;
+}
 
-    
+node* removeKele(node* head , int k){
+    if(head == NULL ) return head;
+
+    if(k==1){
+        node* temp = head;
+        head = head->next;
+        free(temp);
+
+        return head;
+    }
+
+    int cnt = 0;
+    node* temp = head;
+    node* prev = NULL;
+
+    while(temp!=NULL){
+        cnt++;
+
+        if(cnt == k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+
+        prev = temp;
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+node* removeElement(node* head , int ele){
+    if(head==NULL) return head;
+
+    if(head->data == ele){
+        node*temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    node* temp = head;
+    node* prev = NULL;
+
+    while(temp){
+        if(temp->data == ele){
+            prev->next =prev->next->next;
+            free(temp);
+            return head;
+        }
+
+        prev = temp;
+        temp = temp->next;
+    }
+
+    return head;
 }
 
 int main(){
     vector<int> arr = {1,3,4,5,6,7,8};
 
     node* head = convertArr2LL(arr);
-    node* temp = removesTail(head);
+    node* temp = removeElement(head,9);
     print(temp);
     
     return 0;
